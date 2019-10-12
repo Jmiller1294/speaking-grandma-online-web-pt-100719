f3522e46e43bd79b52cef64c24205a4fb7b971f6
Justin Miller <jayman1294@gmail.com>
2019-10-12 01:09:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,6 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts  "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.uppercase
-    puts "NO, NOT SINCE 1938!"
-  else 
-    puts "HUH?! SPEAK UP, SONNY!"
   end
 end
 # Whatever you say to grandma, she should respond with
