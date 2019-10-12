1015b17ba8144888455c73787f8a4e4c54369c3e
Justin Miller <jayman1294@gmail.com>
2019-10-12 01:01:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    puts 
+    puts  "I LOVE YOU TOO PUMPKIN!"
+    binding.pry
   elsif phrase == phrase.uppercase
     puts "NO, NOT SINCE 1938!"
   else 
