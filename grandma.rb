1370c6e353f14b564b809ea7a5df83075ace02a7
Justin Miller <jayman1294@gmail.com>
2019-10-12 01:01:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,12 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if phrase != phrase.uppercase
+  if phrase == "I LOVE YOU GRANDMA!"
     puts 
   elsif phrase == phrase.uppercase
     puts "NO, NOT SINCE 1938!"
   else 
-    puts 
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
